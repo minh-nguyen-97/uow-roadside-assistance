7dfa1c673f3ce43e1fa35b6c87326fd73a0e68f5
AchillesS <qmn1312@gmail.com>
2019-05-21 06:25:12 +1000

    Gather all helper classes that helps to find results in controller

---
+++ uow-roadside-assistance/uow-roadside-assistance/Helper/Contractor/NewWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uow_roadside_assistance.Helper.Contractor
+{
+    public class NewWork : Work
+    {
+        private int _requestID;
+        public int RequestID
+        {
+            get { return _requestID; }
+        }
+
+        private String _responseStatus;
+        public String ResponseStatus
+        {
+            get { return _responseStatus; }
+        }
+
+        public NewWork(String fullName, String cusLat, String cusLng, String conLat, String conLng, String responseStatus, int requestID) : base(fullName, cusLat, cusLng, conLat, conLng)
+        {
+            _requestID = requestID;
+            _responseStatus = responseStatus;
+        }
+    }
+}
---
+++ uow-roadside-assistance/uow-roadside-assistance/Helper/Contractor/Work.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uow_roadside_assistance.Helper.Contractor
+{
+    public class Work
+    {
+        private String _fullName;
+        public String FullName
+        {
+            get { return _fullName; }
+        }
+
+        private String _cusLat;
+        public String CusLat
+        {
+            get { return _cusLat; }
+        }
+
+        private String _cusLng;
+        public String CusLng
+        {
+            get { return _cusLng; }
+        }
+
+        private String _conLat;
+        public String ConLat
+        {
+            get { return _conLat; }
+        }
+
+        private String _conLng;
+        public String ConLng
+        {
+            get { return _conLng; }
+        }
+
+        public Work(String fullName, String cusLat, String cusLng, String conLat, String conLng)
+        {
+            _fullName = fullName;
+            _cusLat = cusLat;
+            _cusLng = cusLng;
+            _conLat = conLat;
+            _conLng = conLng;
+        }
+    }
+}
